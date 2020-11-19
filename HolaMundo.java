784824933d34aec9c15c9f4aa9e7c94b405e8304
Arturo Avendaño <a.avendano04@ufromail.cl>
2020-11-19 14:00:07 -0300

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,5 @@
-public static void main(String[] args){
-  System.out.println("Hola Mundo");
+public class Main{
+  public static void main(String[] args){
+    System.out.println("Hola Mundo");
+    }
   }
